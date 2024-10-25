a81325c6765b15346eaa68fccde3ffddbeb7bf41
rapharamalho <raphael.athenesi@gmail.com>
2024-10-25 16:28:55 -0300

    arrumando a nav

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -8,29 +8,30 @@ $(document).ready(function() {
     const navItems = $('.nav-item');
 
     $(window).on('scroll', function () {
-        const header = $('header');
-        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        const header = $('header'); 
+        const scrollPosition = $(window).scrollTop();
 
         let activeSectionIndex = 0;
 
         if (scrollPosition <= 0) {
             header.css('box-shadow', 'none');
         } else {
-            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
 
         sections.each(function(i) {
             const section = $(this);
             const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop+ section.outerHeight();
+            const sectionBottom = sectionTop + section.outerHeight();
 
             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                activeSectionIndex = i;
-                return false;
+                activeSectionIndex = i; 
+                return false; 
             }
-        })
+        });
 
         navItems.removeClass('active');
+
         $(navItems[activeSectionIndex]).addClass('active');
     });
 
